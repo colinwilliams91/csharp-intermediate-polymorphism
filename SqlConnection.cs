c87527c7169061c5876a896cef015dc8a0ff5a97
colin <colin.williams.dev@gmail.com>
2023-11-13 13:24:47 -0600

    feat: implement base ctor and override methods derrived class

---
+++ SqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_intermediate_polymorphism
+{
+    class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString) {}
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine($"Connection to SQL Database: {this.ConnectionString} opened");
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine($"Connection to SQL Database {this.ConnectionString} closed");
+        }
+    }
+}
