9365a33fb77c6c40a6ce7f2e25dc3a97d9907af2
colin <colin.williams.dev@gmail.com>
2023-11-13 14:30:12 -0600

    feat: run success, polymorphism both classes agnostic

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace csharp_intermediate_polymorphism
         {
             var sql = new SqlConnection("colin/sql/db");
 
-            sql.OpenConnection();
-            sql.CloseConnection();
+            //sql.OpenConnection();
+            //sql.CloseConnection();
+
+            // polymorphism: works with SqlConnection class or OracleConnection class
+            var command = new DbCommand(dbConnection: new OracleConnection("colin/sql/db"), queryType: "INSERT");
+            command.Execute();
         }
     }
 }
