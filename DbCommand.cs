1f0df05558da318a5b1bf5805555929f9c25cb82
colin <colin.williams.dev@gmail.com>
2023-11-13 14:29:59 -0600

    feat: command class receive DbConnection class, handle exceptions

---
+++ DbCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_intermediate_polymorphism
+{
+    class DbCommand
+    {
+        public string Instruction { get; set; }
+        public string QueryType { get; set; }
+        public string CommandType { get; set; }
+        public DbConnection DbConnection { get; set; }
+
+        public DbCommand(DbConnection dbConnection, string queryType)
+        {
+            if (dbConnection == null)
+                throw new ArgumentNullException(nameof(dbConnection));
+
+            if (string.IsNullOrEmpty(dbConnection.DbType) || string.IsNullOrEmpty(queryType))
+                throw new ArgumentNullException();
+
+            this.DbConnection = dbConnection;
+            this.CommandType = dbConnection.DbType;
+            this.QueryType = queryType;
+            this.Instruction = $"Sending {this.CommandType} {this.QueryType} query to DB...";
+        }
+
+        public void Execute()
+        {
+            this.DbConnection.OpenConnection();
+            Console.WriteLine(this.Instruction);
+            this.DbConnection.CloseConnection();
+        }
+    }
+}
