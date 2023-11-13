0413bd6a1faf5f03afe9e06e1891b8587fa4d6a8
colin <colin.williams.dev@gmail.com>
2023-11-13 13:46:18 -0600

    feat: implement Oracle derived class

---
+++ OracleConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_intermediate_polymorphism
+{
+    class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString) {}
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine($"Connection to Oracle Database {this.ConnectionString} opened");
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine($"Connection to Oracle Database {this.ConnectionString} closed");
+        }
+    }
+}
